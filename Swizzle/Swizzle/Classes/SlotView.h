e4ae126dd23a9968ee4f18fc154f0fd0b704dfcd
Sami Aref <sami.aref@smashingideas.com>
2013-05-03 11:32:01 -0700

    slot

---
+++ Swizzle/Swizzle/Classes/SlotView.h
@@ -0,0 +1,13 @@
+//
+//  SlotView.h
+//  Swizzle
+//
+//  Created by Sami Aref on 4/17/13.
+//  Copyright (c) 2013 Lance Hughes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SlotView : UIView
+
+@end
---
+++ Swizzle/Swizzle/Classes/SlotView.m
@@ -0,0 +1,31 @@
+//
+//  SlotView.m
+//  Swizzle
+//
+//  Created by Sami Aref on 4/17/13.
+//  Copyright (c) 2013 Lance Hughes. All rights reserved.
+//
+
+#import "SlotView.h"
+
+@implementation SlotView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
