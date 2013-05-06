ec4e874bde758c51e64a7bd573d099d2c5149a33
Sami Aref <sami.aref@smashingideas.com>
2013-05-06 16:50:08 -0700

    circular slots, needs fixing still

--- Swizzle/Swizzle/Classes/SlotView.h
+++ Swizzle/Swizzle/Classes/SlotView.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SlotView : UIView
+@interface SlotView : UIScrollView
 
 @end
--- Swizzle/Swizzle/Classes/SlotView.m
+++ Swizzle/Swizzle/Classes/SlotView.m
@@ -8,12 +8,15 @@
 
 #import "SlotView.h"
 
-static CGFloat kImageHeight = 66.0f;
+static CGFloat kImageHeight = 100.0f;
+static CGFloat kImageWidth = 87.0f;
 
 @interface SlotView()<UIScrollViewDelegate>
 
-@property (nonatomic, strong) UIScrollView *scrollView;
-@property (nonatomic, strong) NSArray *scrollImageViews;
+@property (nonatomic, strong) NSArray *scrollImageViewNames;
+@property (nonatomic, strong) NSMutableArray *visibleImages;
+@property (nonatomic, strong) UIView *containerView;
+@property NSUInteger imageIndex;
 
 @end
 
@@ -24,37 +27,141 @@ static CGFloat kImageHeight = 66.0f;
     self = [super initWithCoder:aDecoder];
     if (self)
     {
-        
-        _scrollView = [[UIScrollView alloc] initWithFrame:self.bounds];
-        
-        _scrollImageViews = @[
-                              [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"bone_slot"]],
-                              [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"ball_slot"]],
-                              [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"cat_slot"]],
-                              [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"collar_slot"]],
-                              [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"squirrel_slot"]],
-                              [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"3bone_slot"]]
-                              ];
-        
-        CGFloat yPos = 0;
-        
-        for (UIView *view in _scrollImageViews)
-        {
-            view.contentMode = UIViewContentModeScaleAspectFit;
-            view.frame = CGRectMake(0, yPos, CGRectGetWidth(self.bounds), kImageHeight);
-            yPos = CGRectGetMaxY(view.frame);
-            [_scrollView addSubview:view];
-        }
-        
-        _scrollView.contentSize = CGSizeMake(CGRectGetWidth(self.bounds),(kImageHeight * _scrollImageViews.count));
-        _scrollView.showsVerticalScrollIndicator = NO;
-        _scrollView.bounces = NO;
-        
-        [self addSubview:_scrollView];
+        [self initialize];
     }
     
     return self;
 }
 
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+        [self initialize];
+        
+    }
+    return self;
+}
+
+- (void)initialize
+{
+    _scrollImageViewNames = @[@"bone_slot",@"ball_slot"];
+    _visibleImages = [NSMutableArray array];
+    
+    self.contentSize = CGSizeMake(CGRectGetWidth(self.bounds),1000);
+    self.showsVerticalScrollIndicator = NO;
+    self.bounces = NO;
+    
+    _containerView = [[UIView alloc] init];
+    _containerView.frame = CGRectMake(0, 0, self.contentSize.width, self.contentSize.height);
+    [self addSubview:_containerView];
+    _containerView.userInteractionEnabled = NO;
+    
+}
+
+- (void)recenterIfNecessary
+{
+    CGPoint currentOffset = [self contentOffset];
+    CGFloat contentHeight = [self contentSize].height;
+    CGFloat centerOffsetY = (contentHeight - [self bounds].size.height) / 2.0;
+    CGFloat distanceFromCenter = fabs(currentOffset.y - centerOffsetY);
+    
+    if (distanceFromCenter > (contentHeight / 4.0))
+    {
+        self.contentOffset = CGPointMake(currentOffset.x, centerOffsetY);
+        
+        // move content by the same amount so it appears to stay still
+        for (UIImageView *imageView in _visibleImages)
+        {
+            CGPoint center = [_containerView convertPoint:imageView.center toView:self];
+            center.y += (centerOffsetY - currentOffset.y);
+            imageView.center = [self convertPoint:center toView:_containerView];
+        }
+    }
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    [self recenterIfNecessary];
+    
+    // tile content in visible bounds
+    CGRect visibleBounds = [self convertRect:[self bounds] toView:_containerView];
+    CGFloat minimumVisibleY = CGRectGetMinY(visibleBounds);
+    CGFloat maximumVisibleY = CGRectGetMaxY(visibleBounds);
+    
+    [self imagesFromMinY:minimumVisibleY toMaxY:maximumVisibleY];
+}
+
+- (void)imagesFromMinY:(CGFloat)minimumVisibleY toMaxY:(CGFloat)maximumVisibleY {
+    
+    // the upcoming tiling logic depends on there already being at least one label in the visibleLabels array, so
+    // to kick off the tiling we need to make sure there's at least one label
+    if ([_visibleImages count] == 0)
+    {
+        [self placeNewImageOnBottom:minimumVisibleY];
+    }
+    
+    // add labels that are missing on top side
+    UIImageView *lastImageView = [_visibleImages lastObject];
+    CGFloat bottomEdge = CGRectGetMaxY([lastImageView frame]);
+    while (bottomEdge < maximumVisibleY)
+    {
+        bottomEdge = [self placeNewImageOnBottom:bottomEdge];
+    }
+    
+    // add labels that are missing on bottom side
+    UIImageView *firstImageView = [_visibleImages objectAtIndex:0];
+    CGFloat topEdge = CGRectGetMinY([firstImageView frame]);
+    while (topEdge > minimumVisibleY)
+    {
+        topEdge = [self placeNewImageOnTop:topEdge];
+    }
+    
+    // remove labels that have fallen off right edge
+    lastImageView = [_visibleImages lastObject];
+    while ([lastImageView frame].origin.y > maximumVisibleY)
+    {
+        [lastImageView removeFromSuperview];
+        [_visibleImages removeLastObject];
+        lastImageView = [_visibleImages lastObject];
+    }
+    
+    // remove labels that have fallen off left edge
+    firstImageView = [_visibleImages objectAtIndex:0];
+    while (CGRectGetMaxY([firstImageView frame]) < minimumVisibleY)
+    {
+        [firstImageView removeFromSuperview];
+        [_visibleImages removeObjectAtIndex:0];
+        firstImageView = [_visibleImages objectAtIndex:0];
+    }
+}
+- (UIImageView *)insertImageAtYPos:(CGFloat)yPos
+{
+    NSLog(@"ypos:%f , ratio: %f",yPos,(yPos / kImageHeight));
+    int index = yPos / kImageHeight;
+    index %= _scrollImageViewNames.count;
+     NSLog(@"index: %d",index);
+    NSString *imageViewName = _scrollImageViewNames[index];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:imageViewName]];
+    imageView.frame = CGRectMake(0, yPos, kImageWidth, kImageHeight);
+    [_containerView addSubview:imageView];
+    return imageView;
+}
+
+- (CGFloat)placeNewImageOnBottom:(CGFloat)bottomEdge
+{
+    UIImageView *imageView = [self insertImageAtYPos:bottomEdge];
+    [_visibleImages addObject:imageView]; // add rightmost label at the end of the array
+    return CGRectGetMaxY(imageView.frame);
+}
+
+- (CGFloat)placeNewImageOnTop:(CGFloat)topEdge
+{
+    UIImageView *imageView = [self insertImageAtYPos:(topEdge - kImageHeight)];
+    [_visibleImages insertObject:imageView atIndex:0]; // add leftmost label at the beginning of the array
+    return CGRectGetMinY(imageView.frame);
+}
 
 @end
